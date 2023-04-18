06a3c331d7f45f6e0186d42419e1da6dd8b054cb
MateuszPoterala <poteralamateusz@gmail.com>
2023-04-18 18:34:33 +0200

    init first StringBoot project

---
+++ src/test/java/pl/sda/OrangeJavaPL2Spring/OrangeJavaPl2SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.OrangeJavaPL2Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeJavaPl2SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
