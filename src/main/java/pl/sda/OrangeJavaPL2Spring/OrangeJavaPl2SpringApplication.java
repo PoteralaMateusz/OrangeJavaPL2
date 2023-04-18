51d47958912143a21abb9befe178279475bd99f3
MateuszPoterala <poteralamateusz@gmail.com>
2023-04-18 18:39:22 +0200

    init first StringBoot project

---
+++ src/main/java/pl/sda/OrangeJavaPL2Spring/OrangeJavaPl2SpringApplication.java
@@ -0,0 +1,14 @@
+package pl.sda.OrangeJavaPL2Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeJavaPl2SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangeJavaPl2SpringApplication.class, args);
+		//hello
+	}
+
+}
