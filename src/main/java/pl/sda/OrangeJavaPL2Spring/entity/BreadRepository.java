3e4a8b177c3567c8548568b1fdd36ae80bb6018e
MateuszPoterala <poteralamateusz@gmail.com>
2023-04-22 10:50:02 +0200

    add delete and get bread by price

--- src/main/java/pl/sda/OrangeJavaPL2Spring/entity/BreadRepository.java
+++ src/main/java/pl/sda/OrangeJavaPL2Spring/entity/BreadRepository.java
@@ -3,6 +3,10 @@ package pl.sda.OrangeJavaPL2Spring.entity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BreadRepository extends JpaRepository<Bread,Integer> {
+
+    Optional<Bread> getBreadByPrice(double price);
 }
--- src/main/java/pl/sda/OrangeJavaPL2Spring/restapi/BreadController.java
+++ src/main/java/pl/sda/OrangeJavaPL2Spring/restapi/BreadController.java
@@ -5,6 +5,7 @@ import pl.sda.OrangeJavaPL2Spring.entity.Bread;
 import pl.sda.OrangeJavaPL2Spring.entity.BreadRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class BreadController {
@@ -19,9 +20,21 @@ public class BreadController {
         return repository.findAll();
     }
 
+    @GetMapping("/bread_by_price/{price}")
+    public Bread getBreadByPrice(@PathVariable double price){
+        Optional<Bread> breadByPrice = repository.getBreadByPrice(price);
+        return breadByPrice.orElse(null);
+    }
+
     @PostMapping("/breads")
     public void createBread(@RequestBody Bread bread){
         repository.save(bread);
+    }
+
+    @DeleteMapping("/breads/{id}")
+    public void deleteBread(@PathVariable int id){
+        Optional<Bread> breadToDelete = repository.findById(id);
+        breadToDelete.ifPresent(bread -> repository.delete(bread));
     }
 
 }
