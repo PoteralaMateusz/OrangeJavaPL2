fc7734e94da559065b7173c61e51015ec965a1fb
MateuszPoterala <poteralamateusz@gmail.com>
2023-04-22 11:37:26 +0200

    some refactor

--- src/main/java/pl/sda/OrangeJavaPL2Spring/entity/IBreadRepository.java
+++ src/main/java/pl/sda/OrangeJavaPL2Spring/entity/IBreadRepository.java
@@ -1,7 +1,7 @@
 package pl.sda.OrangeJavaPL2Spring.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import java.util.List;
 import java.util.Optional;
