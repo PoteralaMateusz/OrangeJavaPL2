c32415031cad893cc1281629808fd3474d3be8f5
MateuszPoterala <poteralamateusz@gmail.com>
2023-04-22 10:51:06 +0200

    some change

--- src/main/java/pl/sda/OrangeJavaPL2Spring/restapi/MyFirstRestController.java
+++ src/main/java/pl/sda/OrangeJavaPL2Spring/restapi/MyFirstRestController.java
@@ -1,7 +1,6 @@
 package pl.sda.OrangeJavaPL2Spring.restapi;
 
 import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
